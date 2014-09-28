e2f58cbef4364550894d7483ac935e118777d8eb
Jason Hsu <jhsu@debian.debianserver.com>
2014-09-28 12:06:55 -0500

    Version 0.0.1, should -> expect

--- spec/lib/array_in_file_spec.rb
+++ spec/lib/array_in_file_spec.rb
@@ -5,7 +5,7 @@ describe ArrayInFile do
   it "should return the array contents of a file" do
     file_to_read = File.expand_path("../file_to_read.txt", __FILE__)
     array_to_read = ArrayInFile.read(file_to_read)
-    array_to_read.should == ["Doppler Value Investing", "Bargain Stock Funds"]
+    expect(array_to_read).to eq(["Doppler Value Investing", "Bargain Stock Funds"])
   end
   
   it "should write the array contents to a file" do
@@ -13,7 +13,7 @@ describe ArrayInFile do
     array_to_write = ["Warren Buffett", "Charlie Munger"]
     ArrayInFile.write(array_to_write, file_to_write)
     array_to_read = ArrayInFile.read(file_to_write)
-    array_to_read.should == array_to_write
+    expect(array_to_read).to eq(array_to_write)
   end
   
 end
